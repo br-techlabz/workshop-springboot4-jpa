2526ec72602d80c0b83a9009e71143ae31a6cdb0
willian <br.techlabz@proton.me>
2026-04-21 12:15:09 -0300

    Criação do projeto

---
+++ src/main/java/br/techlabz/curso_springboot_nelioalves/CursoSpringbootNelioalvesApplication.java
@@ -0,0 +1,13 @@
+package br.techlabz.curso_springboot_nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringbootNelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringbootNelioalvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/techlabz/curso_springboot_nelioalves/CursoSpringbootNelioalvesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.techlabz.curso_springboot_nelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootNelioalvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
