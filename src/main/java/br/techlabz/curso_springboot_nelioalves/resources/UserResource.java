76d6f70210ac2ab43929ae54284ef381a994f667
willian <br.techlabz@proton.me>
2026-04-21 12:51:49 -0300

    implementação Endpoint de usuário

--- src/main/java/br/techlabz/curso_springboot_nelioalves/resources/UserResource.java
+++ src/main/java/br/techlabz/curso_springboot_nelioalves/resources/UserResource.java
@@ -1,5 +1,21 @@
 package br.techlabz.curso_springboot_nelioalves.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.techlabz.curso_springboot_nelioalves.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User>findAll(){
+		User u = new User(1L,"Willian","will@email.com","999-99999","senha123");
+		
+		return ResponseEntity.ok().body(u);
+	}
 	
 }
