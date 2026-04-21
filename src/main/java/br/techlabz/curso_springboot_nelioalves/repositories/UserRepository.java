331b861335c25adb3bafc64312ae6d74009312a9
willian <br.techlabz@proton.me>
2026-04-21 15:34:35 -0300

    Implementação da camada de serviço

--- src/main/java/br/techlabz/curso_springboot_nelioalves/repositories/UserRepository.java
+++ src/main/java/br/techlabz/curso_springboot_nelioalves/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.techlabz.curso_springboot_nelioalves.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.techlabz.curso_springboot_nelioalves.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/br/techlabz/curso_springboot_nelioalves/services/UserService.java
@@ -0,0 +1,30 @@
+package br.techlabz.curso_springboot_nelioalves.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.techlabz.curso_springboot_nelioalves.entities.User;
+import br.techlabz.curso_springboot_nelioalves.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+
+}
