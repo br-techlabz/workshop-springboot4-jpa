b6101780cb07f637598da4ccc4995083aa32fdbd
willian <br.techlabz@proton.me>
2026-04-26 10:13:24 -0300

    Tratamento de erros personalizado para User FindById

--- src/main/java/br/techlabz/curso_springboot_nelioalves/services/UserService.java
+++ src/main/java/br/techlabz/curso_springboot_nelioalves/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.techlabz.curso_springboot_nelioalves.entities.User;
 import br.techlabz.curso_springboot_nelioalves.repositories.UserRepository;
+import br.techlabz.curso_springboot_nelioalves.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,9 +23,8 @@ public class UserService {
 	}
 	
 	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
-		
-		return obj.get();
+		Optional<User> obj = repository.findById(id);		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
